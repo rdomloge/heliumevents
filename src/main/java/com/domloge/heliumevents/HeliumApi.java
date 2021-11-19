1bc92af83c4b418aeeeac64b2b082f86b0201e18
Ramsay Domloge <rdomloge@gmail.com>
2021-11-19 00:48:05 +0000

    Next draft

--- src/main/java/com/domloge/heliumevents/HeliumApi.java
+++ src/main/java/com/domloge/heliumevents/HeliumApi.java
@@ -43,12 +43,47 @@ public class HeliumApi {
     private String HS_ACTIVITY_DATA;
     private String HS_DETAILS;
 
-    private static final DateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'"); //2021-05-11T01:39:53Z
+    private static final DateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss"); //2021-05-11T01:39:53Z
 
-    private HttpClient client = HttpClient.newHttpClient();
+    @Value("${USER_AGENT:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36}")
+    private String USER_AGENT;
+
+    @Value("${INTERVAL:500}")
+    private long interval;
+
+    private HttpClient _client = HttpClient.newHttpClient();
 
     private JsonObject hotspotDetails;
     
+    private HttpResponse<String> sendRequest(String url) throws HeliumApiException {
+
+        HttpRequest req = HttpRequest.newBuilder()
+            .uri(URI.create(url))
+            .setHeader("user-agent", USER_AGENT)
+            .build();
+
+        HttpResponse<String> resp;
+        try {
+            resp = _client.send(req, BodyHandlers.ofString());
+        } 
+        catch (IOException | InterruptedException e) {
+            throw new HeliumApiException("Failed to call Helium API", e);
+        }
+
+        if(resp.statusCode() != 200) {
+            throw new HeliumApiException("Got response code "+resp.statusCode()+ " for URL "+url+ " and response "+resp.body());
+        }
+        
+        try {
+            Thread.sleep(interval);
+        } 
+        catch (InterruptedException e) {
+            throw new HeliumApiException("Interrupted");
+        }
+
+        return resp;
+    }
+
     @PostConstruct
     public void confid() {
         if(useHeliumApi && useStakejoyApi) throw new IllegalStateException("Can't use both APIs - choose one");
@@ -69,25 +104,20 @@ public class HeliumApi {
     }
 
 
-    public String getHotspotName(String hotspotAddress) throws IOException, InterruptedException {
+    public String getHotspotName(String hotspotAddress) throws HeliumApiException {
         if(null == hotspotDetails) initHotspotDetails(hotspotAddress);
         return hotspotDetails.get("data").getAsJsonObject().get("name").getAsString();
     }
 
-    public DateTime getHotspotBirithday(String hotspotAddress) throws IOException, InterruptedException {
+    public DateTime getHotspotBirithday(String hotspotAddress) throws HeliumApiException {
         if(null == hotspotDetails) initHotspotDetails(hotspotAddress);
         String timestamp = hotspotDetails.get("data").getAsJsonObject().get("timestamp_added").getAsString();
         return DateTime.parse(timestamp); // "2021-09-20T11:22:46.000000Z"
     }
 
-    private void initHotspotDetails(String hotspotAddress) throws IOException, InterruptedException {
+    private void initHotspotDetails(String hotspotAddress) throws HeliumApiException {
         String url = String.format(HS_DETAILS, hotspotAddress);
-        HttpRequest req = HttpRequest.newBuilder().uri(URI.create(url)).build();
-        HttpResponse<String> resp = client.send(req, BodyHandlers.ofString());
-        if(resp.statusCode() != 200) {
-            throw new RuntimeException("Got response code "+resp.statusCode()+ " for URL "+url+ " and response "+resp.body());
-        }
-        String json = resp.body();
+        String json = sendRequest(url).body();
         hotspotDetails = JsonParser.parseString(json).getAsJsonObject();
     }
 
@@ -100,7 +130,7 @@ public class HeliumApi {
      * @throws IOException
      * @throws InterruptedException
      */
-    String fetchHotspotActivityCursorForDate(String hotspotAddress, DateTime date) throws IOException, InterruptedException {
+    JsonObject fetchHotspotActivityForDate(String hotspotAddress, DateTime date) throws HeliumApiException {
         // Build the URL
         String min_time = format.format(date.withTime(0, 0, 0, 0).toDate());
         String max_time = format.format(date.withTime(23, 59, 59, 999).toDate());
@@ -109,34 +139,21 @@ public class HeliumApi {
         String url = String.format(HS_ACTIVITY_CURSOR, hotspotAddress, limit, filter_types, min_time, max_time);
 
         // Process the response and extract the cursor hash
-        HttpRequest req = HttpRequest.newBuilder().uri(URI.create(url)).build();
-        HttpResponse<String> resp = client.send(req, BodyHandlers.ofString());
-        if(resp.statusCode() != 200) {
-            throw new RuntimeException("Got response code "+resp.statusCode()+ " for URL "+url+ " and response "+resp.body());
-        }
+        HttpResponse<String> resp = sendRequest(url);
+
         String json = resp.body();
         JsonObject jsObj = JsonParser.parseString(json).getAsJsonObject();
         if(jsObj.has("error")) {
-            throw new RuntimeException(jsObj.get("error").getAsString());
-        }
-        if( ! jsObj.has("cursor")) {
-            return null; // this can happen when there is no activity (just after midnight)
+            throw new HeliumApiException(jsObj.get("error").getAsString());
         }
 
-        return jsObj.get("cursor").getAsString();
+        return jsObj;
     }
 
-    public JsonArray fetchTransactions(String hotspotAddress, String cursor, String hotspotName) 
-            throws IOException, InterruptedException {
-        
+    public JsonArray fetchTransactions(String hotspotAddress, String cursor, String hotspotName) throws HeliumApiException {
         // Fetch the data
         String url = String.format(HS_ACTIVITY_DATA, hotspotAddress, cursor);
-        HttpRequest req = HttpRequest.newBuilder().uri(URI.create(url)).build();
-        HttpResponse<String> resp = client.send(req, BodyHandlers.ofString());
-
-        if(resp.statusCode() != 200) {
-            throw new RuntimeException("Got response code "+resp.statusCode()+ " for URL "+url+ " and response "+resp.body());
-        }
+        HttpResponse<String> resp = sendRequest(url);
 
         // Process the transactions
         String json = resp.body();
--- src/main/java/com/domloge/heliumevents/Trawler.java
+++ src/main/java/com/domloge/heliumevents/Trawler.java
@@ -38,15 +38,21 @@ public class Trawler {
     private String hotspotName;
 
     @PostConstruct
-    void prepAndRun() throws IOException, InterruptedException {
+    void prepAndRun() {
         
-        hotspotName = heliumApi.getHotspotName(hotspot);
+        try {
+            hotspotName = heliumApi.getHotspotName(hotspot);
+        } 
+        catch (HeliumApiException hex) {
+            handleError(hex);
+            return;
+        }
         
         // check that the metadata index exists
-        ResponseEntity<String> metadataJson = esApi.getRaw("/metadataindex/_doc/metadata");
+        ResponseEntity<String> metadataJson = esApi.getRaw("/metadataindex/_doc/"+hotspotName);
         if(metadataJson.getStatusCode() != HttpStatus.OK) {
             logger.info("Setting up metadata index");
-            esApi.postDoc("metadataindex", "metadata", "{ \"position\": 0 }");
+            esApi.postDoc("metadataindex", hotspotName, "{ \"position\": 0 }");
         }
 
         // create mapping for timestamp in documents
@@ -60,62 +66,76 @@ public class Trawler {
     }
 
 
-    public void trawl() throws IOException, InterruptedException {
-        DateTime hsBday = heliumApi.getHotspotBirithday(hotspot);
-        DateTime latestTrawlCompleteDay = getLatestSuccessfulTrawlCompleteDay();
-        if(null == latestTrawlCompleteDay) latestTrawlCompleteDay = hsBday;
-        latestTrawlCompleteDay = latestTrawlCompleteDay.plusDays(-1); // go back a day  to make sure we get a full day
-        
-        
-        logger.info("Synching from {} for hotspot {}, born on {}", latestTrawlCompleteDay, hotspotName, hsBday);
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
-        DateTime dateCursor = latestTrawlCompleteDay;
-        
-        while(dateCursor.isBefore(new DateTime())) {
-            logger.info("Fetching events for {}", dateCursor.toString());
-            String cursor = heliumApi.fetchHotspotActivityCursorForDate(hotspot, dateCursor);
-            if(null != cursor) {
-
-                JsonArray transactions = heliumApi.fetchTransactions(
-                    hotspot, 
-                    cursor,
-                    hotspotName);
-
-                for(int i=0; i < transactions.size(); i++) {
-                    JsonObject doc = transactions.get(i).getAsJsonObject();
-                    patch(doc);
-        
-                    String identifier = doc.get("hash").getAsString();
-                    String docStr = new Gson().toJson(doc);
-                    logger.trace(docStr);
-                    
-                    esApi.postDoc(hotspotName, identifier, docStr);
-                }
-                storeMetadata(dateCursor);
-                logger.debug("Added {} docs for date", transactions.size());
-            }
-            else {
-                logger.info("No transactions on {}", dateCursor.toString());
-            }
+    public void trawl() {
+        try {
+            DateTime hsBday = heliumApi.getHotspotBirithday(hotspot);
+            DateTime latestTrawlCompleteDay = getLatestSuccessfulTrawlCompleteDay(hotspotName);
+            if(null == latestTrawlCompleteDay) latestTrawlCompleteDay = hsBday;
             
-            dateCursor = dateCursor.plusDays(1);
+            logger.info("Synching from {} for hotspot {}, born on {}", latestTrawlCompleteDay, hotspotName, hsBday);
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+            DateTime dateCursor = latestTrawlCompleteDay;
+            
+            while(dateCursor.isBefore(new DateTime())) {
+                logger.info("Fetching events for {}", dateCursor.toString());
+                JsonObject response = heliumApi.fetchHotspotActivityForDate(hotspot, dateCursor);
+                if(response.has("cursor")) {
+                    JsonArray transactions = heliumApi.fetchTransactions(
+                        hotspot, 
+                        response.get("cursor").getAsString(),
+                        hotspotName);
+                    processData(transactions);
+                }
+                if(response.has("data")) {
+                    processData(response.getAsJsonArray("data"));
+                }
+                
+                storeMetadata(hotspotName, dateCursor);
+                dateCursor = dateCursor.plusDays(1);
+            }
+            logger.info("Synch complete");
         }
-        logger.info("Synch complete");
+        catch(HeliumApiException hex) {
+            handleError(hex);
+        }
+    }
+
+    private void handleError(HeliumApiException hex) {
+        logger.info("**************************");
+        String message = hex.getCause() != null ? hex.getCause().getMessage() : hex.getMessage();
+        logger.info("Critical error calling Helium API: {}", message);
+        if(logger.isDebugEnabled()) {
+            logger.error("Error details:\n", hex);
+        }
+    }
+
+    private void processData(JsonArray transactions) {
+        for(int i=0; i < transactions.size(); i++) {
+            JsonObject doc = transactions.get(i).getAsJsonObject();
+            patch(doc);
+
+            String identifier = doc.get("hash").getAsString();
+            String docStr = new Gson().toJson(doc);
+            logger.trace(docStr);
+            
+            esApi.postDoc(hotspotName, identifier, docStr);
+        }
+        logger.debug("Added {} docs for date", transactions.size());
     }
 
     private static final String LAST_RUN_DATE = "lastRun";
 
-    private void storeMetadata(DateTime lastRunDate) {
+    private void storeMetadata(String hotspotName, DateTime lastRunDate) {
         JsonObject metadata = new JsonObject();
         metadata.add(LAST_RUN_DATE, new JsonPrimitive(lastRunDate.getMillis()));
         metadata.add(LAST_RUN_DATE+"HumanReadable", new JsonPrimitive(lastRunDate.toString()));
-        esApi.postDoc("metadataindex", "metadata", metadata.toString());
+        esApi.postDoc("metadataindex", hotspotName, metadata.toString());
     }
 
-    private DateTime getLatestSuccessfulTrawlCompleteDay() {
-        JsonObject metadata = esApi.getDoc("metadataindex", "metadata");
+    private DateTime getLatestSuccessfulTrawlCompleteDay(String hotspotName) {
+        JsonObject metadata = esApi.getDoc("metadataindex", hotspotName);
         if(null == metadata || ! metadata.has(LAST_RUN_DATE)) return null;
         return new DateTime(metadata.get(LAST_RUN_DATE).getAsLong());
     }
