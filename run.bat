2cd09a461c6d9383c24c4603104fb8d817d545ea
Ramsay Domloge <rdomloge@gmail.com>
2022-01-25 15:57:11 +0000

    Removed some odl leftover stuff and made an initial crack at the elastalert Dockerfile

--- run.bat
+++
@@ -1,9 +0,0 @@
-set VER=1.0.3
-set JAMES=112KVEvBawFScQeXgmzLGAZLG3mAozQFchig3NzPKBi5FYKvQGdk
-set ME=112Xa4p36ExdVDktAPFKx9zd8EwiMw7vC35hxyqiiYANC527BLiF
-
-FOR /L %%A IN (1,1,200) DO (
-  rem docker run -ti --name heliumevents --rm --net elasticsearch -e ES_SERVER_ADDRESS=http://elasticsearch:9200 -e USE_STAKEJOY_API=true -e HOTSPOT=%ME% rdomloge/heliumevents:%VER%  
-  docker run -ti --name heliumevents --rm --net elasticsearch -e ES_SERVER_ADDRESS=http://elasticsearch:9200 -e USE_STAKEJOY_API=true -e HOTSPOT=%JAMES% rdomloge/heliumevents:%VER%
-)
-
--- runDocker.bat
+++
@@ -1,5 +0,0 @@
-docker run -t --name heliumevents\
- --net elasticsearch --rm\
-  -e ES_SERVER_ADDRESS=http://elasticsearch:9200\
-   -e HOTSPOT=112Xa4p36ExdVDktAPFKx9zd8EwiMw7vC35hxyqiiYANC527BLiF\
-    rdomloge/heliumevents
