deee62357c48d91c6f66406fa514b0b8cda391bd
Ramsay Domloge <rdomloge@gmail.com>
2022-01-31 19:27:51 +0000

    Another stab at getting ElastAlert2 going

--- monitoring/build-elastalert2.sh
+++
@@ -1,1 +0,0 @@
-docker buildx build --platform linux/amd64,linux/arm64 -t rdomloge/elastalert:1.0 . --push
--- monitoring/run-elastalert2.sh
+++ monitoring/run-elastalert2.sh
@@ -1,4 +1,4 @@
-CWD=$(pwd) && docker run -ti --mount type=bind,source=$CWD/my-config.yaml,\
-target=/opelastalert/config.yaml -v \
-$CWD/elastalert2-alerts:/opt/elastalert/examples/rules \
+CWD=$(pwd) && docker run -ti --mount \
+type=bind,source=$CWD/my-config.yaml,target=/opt/elastalert/config.yaml \
+--mount type=bind,source=$CWD/elastalert2-alerts,target=/opt/elastalert/rules \
 jertel/elastalert2
