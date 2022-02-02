12a4cef980a4df45c8e53dbafe97d43a46a73f52
Ramsay Domloge <rdomloge@gmail.com>
2022-02-02 22:28:50 +0000

    Trying to make the JSON compliant for Discord

--- monitoring/heliumevents-alerting/Range.py
+++ monitoring/heliumevents-alerting/Range.py
@@ -44,11 +44,11 @@ fieldValue = inputdoc[field]
 print("{} value {}".format(field, fieldValue))
 if(fieldValue < min):
     print("{} {} is too low".format(field, fieldValue))
-    requests.get(webhook, headers={"Content-Type": "application/json"}, data = {"username":"Range issue", "content": "{} is lower than {} :: {}".format(field, min, fieldValue), verify:ignoreSslErrors})
+    requests.post(webhook, headers={"Content-Type": "application/json"}, data = {"username":"Range issue", "content": "{} is lower than {} - {}".format(field, min, fieldValue), verify:ignoreSslErrors})
 elif(fieldValue > max):
     print("{} {} is too high".format(field, fieldValue))
-    response = requests.get(webhook, 
-        data = {"username":"Range issue", "content": "{} is higher than {} :: {}".format(field, max, fieldValue)},
+    response = requests.post(webhook, 
+        data = {"username":"Range issue", "content": "{} is higher than {} {}".format(field, max, fieldValue)},
         headers={"content-type": "application/json"})
     print("Webhook called '{}' code, '{}' message".format(response.status_code, response.content))
 else:
