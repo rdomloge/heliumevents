3fb9574b3c5e60923e9ace461f5a7e1a3dab7c63
Ramsay Domloge <rdomloge@gmail.com>
2022-02-07 14:30:00 +0000

    Got rid of the remaining elastalert stuff

--- monitoring/run-elastalert2.sh
+++
@@ -1,4 +0,0 @@
-CWD=$(pwd) && docker run -ti --mount \
-type=bind,source=$CWD/my-config.yaml,target=/opt/elastalert/config.yaml \
---mount type=bind,source=$CWD/elastalert2-alerts,target=/opt/elastalert/rules \
-jertel/elastalert2
